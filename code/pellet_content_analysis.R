8a773e5d363d0cb6faa1fba0de7e3988cb8a9aa7
decastro-j <jldecastro@ucsb.edu>
2026-05-26 00:00:43 -0700

    Updated code for figures and recalculated percent pellet occurrence data subsets.

--- code/pellet_content_analysis.R
+++ code/pellet_content_analysis.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(readxl)
 library(cowplot)
 library(rphylopic)
+library(ggtext)
 
 # Read in data ----
 # read in ID_Counting sheet from Excel and assign to variable & exclude NCOS pellet data.
@@ -47,8 +48,9 @@ proportions <- count_sums %>%
   mutate(invert_proportion = Invertebrate/total_prey) %>%
   mutate(Site = "Dangermond")
 
-# Calculate total proportion of invertebrate prey across all pellets
+# Calculate total proportions of prey across all pellets
 mean_invert_prop <- mean(proportions$invert_proportion)
+mean_vert_prop <- mean(proportions$vert_proportion)
 
 # NCOS Data Exploration ----
 # check catalog numbers
@@ -74,6 +76,7 @@ NCOS_proportions <- NCOS_count_sums %>%
 
 # Calculate total proportion of invertebrate prey across all pellets
 NCOS_mean_invert_prop <- mean(NCOS_proportions$invert_proportion)
+NCOS_mean_vert_prop <- mean(NCOS_proportions$vert_proportion)
 
 ## Create taxon-based data frame for count of individuals ----
 prey_taxa_lumped <- pellet_contents %>%
@@ -90,13 +93,13 @@ prey_taxa_lumped <- pellet_contents %>%
   group_by(common_name, `vert-invert`) %>%
   summarise(across(where(is.numeric), sum))
 
-## Create taxon-based data frame for occurrence frequency in pellets ----
+## Create common-name-based data frame for occurrence frequency in pellets ----
 # Lump together instances of taxa by how many times they appear in pellets
 prey_occ <- pellet_contents %>% 
-  select(catalog_number, prey_taxon, common_name, `vert-invert`) %>%
+  select(catalog_number, common_name, `vert-invert`) %>%
   group_by(common_name, `vert-invert`) %>%
   count(common_name) %>%
-  mutate(freq_occ = (n / 25)*100) %>%
+  mutate(freq_occ = (n / n_distinct(pellet_contents$catalog_number))*100) %>%
   filter(common_name != "Insects") %>%
 # Group together/collapse repetitive/higher resolution taxa categories
   mutate(common_name = case_when(common_name %in% c("Rodent") ~ "Unidentified Rodent", TRUE ~ common_name), 
@@ -105,9 +108,37 @@ prey_occ <- pellet_contents %>%
          common_name = case_when(common_name %in% c("Grasshoppers, locusts, crickets", "Jerusalem crickets") ~ "Grasshoppers, locusts, crickets",TRUE ~ common_name)) %>%
   group_by(common_name, `vert-invert`) %>%
   summarise(across(where(is.numeric), sum))
-  
-view(prey_occ)
 
+## Create a scientific-name-based taxon data frame for occurrence frequency ----
+# Separate inverts and lump them by taxonomic order per pellet occurrence
+inv_prey_occ <- pellet_contents %>%
+  select(catalog_number, taxon_order, `vert-invert`) %>%
+  group_by(catalog_number, taxon_order, `vert-invert`) %>%
+  count(taxon_order) %>% # Lump together each occurring order
+  mutate(n = ifelse(n > 1,1,n)) %>% # Renumber instances of taxa to 1 to count presence in a pellet once; account for duplicate instances. 
+  filter(taxon_order != "Insecta", `vert-invert` == "Invertebrate") %>% # Isolate invertebrates for purely order-based lumping
+  mutate(freq_occ = (n/n_distinct(pellet_contents$catalog_number)*100)) %>%
+  group_by(taxon_order, `vert-invert`) %>%
+  summarise(across(where(is.numeric), sum)) %>%
+  rename(prey_taxon = taxon_order)
+
+# Separate verts and lump by finest taxonomic resolution 
+vert_prey_occ <- pellet_contents %>%
+  select(catalog_number, prey_taxon, `vert-invert`) %>%
+  group_by(catalog_number, prey_taxon, `vert-invert`) %>%
+  filter(`vert-invert` == "Vertebrate") %>%
+  count(prey_taxon) %>% #
+  mutate(n = ifelse(n > 1,1,n)) %>% # Renumber instances of taxa to 1 to count presence in a pellet once; account for duplicate instances
+  mutate(freq_occ = (n/n_distinct(pellet_contents$catalog_number)*100)) %>%
+  group_by(prey_taxon, `vert-invert`) %>%
+  summarise(across(where(is.numeric), sum))
+
+sci_prey_occ <- rbind(inv_prey_occ, vert_prey_occ) %>% # Combine the lumped occurrence data subsets
+  mutate(prey_taxon = case_when(prey_taxon %in% "Coleoptera" ~ "Coleoptera spp.", TRUE ~ prey_taxon),
+         prey_taxon = case_when(prey_taxon %in% "Orthoptera" ~ "Orthoptera spp.", TRUE ~ prey_taxon),
+         prey_taxon = case_when(prey_taxon %in% "Dermaptera" ~ "Dermaptera spp.", TRUE ~ prey_taxon),
+         prey_taxon = case_when(prey_taxon %in% "Hemiptera" ~ "Hemiptera spp.", TRUE ~ prey_taxon))
+  
 # Figures ----
 ## Add images from phylopic ----
 ## Pick out the appropriate images
@@ -127,7 +158,7 @@ img_buprestidae <- pick_phylopic(name = "Buprestidae", n = 4, view = 4) # Select
 img_peromyscus <- pick_phylopic(name = "Peromyscus", n = 4, view = 4) # Select image 2
 img_curculionidae <- pick_phylopic(name = "Curculionidae", n = 4, view = 4) # Select image 2
 
-## Total # individuals bar chart ----
+## Total # individuals bar chart by common name ----
 fig_prey_taxa <- ggplot(prey_taxa_lumped, 
                         aes(x = MNI_tot, 
                             y = reorder(common_name, MNI_tot), 
@@ -142,7 +173,7 @@ fig_prey_taxa <- ggplot(prey_taxa_lumped,
         legend.position.inside = c(0.75,0.75),
         legend.title = element_text(size = 15, face = "bold"),
         legend.text = element_text(size = 12),
-        legend.box.background = element_rect(size = 2, color = "#003660ff")) +
+        legend.box.background = element_rect(linewidth = 2, color = "#003660ff")) +
   # Add phylopic icons to the figure
   ## Dermaptera
   add_phylopic(img_dermaptera, x = 67, y = 15, 
@@ -225,7 +256,7 @@ ggsave(filename = "figures/fig_prey_taxa.png",
        width = 8,
        height = 7)
 
-## Prey occurrence in pellets bar chart ---- 
+## Prey occurrence in pellets bar chart by common name ---- 
 fig_prey_occ <- ggplot(prey_occ, 
                        aes(x = freq_occ, 
                            y = reorder(common_name, freq_occ), 
@@ -327,6 +358,87 @@ ggsave(filename = "figures/fig_prey_occ.png",
        width = 8,
        height = 7)
 
+## Prey occurrence in pellets bar chart by scientific name ----
+fig_sci_prey_occ <- ggplot(sci_prey_occ, 
+                       aes(x = freq_occ, 
+                           y = reorder(prey_taxon, freq_occ), 
+                           fill = `vert-invert`)) +
+  geom_bar(stat = "identity", color = "#003660ff") + 
+  xlim(0, 100) +
+  scale_fill_manual(values = c("#003660ff", "#87f7cbff")) +
+  theme_bw() +
+  labs(title = "Taxa occurrence in pellet samples", 
+       x = "% Occurrence in Pellets", y = "Prey Taxon", fill = "Classification") +
+  
+  # TODO Italicize the species names where applicable
+  theme(title = element_text(face = "bold", size = 18, 
+                                  family = "serif", color = "#003660ff"),
+        axis.title = element_text(face = "bold", size = 15, 
+                                  family = "serif", color = "#003660ff"),
+        axis.text = element_text(size = 15, family = "serif"),
+        legend.position = "inside",
+        legend.position.inside = c(0.75,0.15),
+        legend.title = element_text(size = 18, face = "bold",
+                                    family = "serif", color = "#003660ff"),
+        legend.text = element_text(size = 15, family = "serif"),
+        legend.box.background = element_rect(linewidth = 2, color = "#003660ff")) +
+  
+  # Add phylopic icons to the figure
+  ## Coleoptera
+  add_phylopic(img = rotate_phylopic(img_coleoptera, angle = 90), 
+               x = 90, y = 9, 
+               height = 0.70, fill = "#003660ff") +
+  ## Microtus
+  add_phylopic(img_microtus, x = 77, y = 8, 
+               height = 0.60, fill = "#87f7cbff", color = "#003660ff") +
+  ## Dermaptera
+  add_phylopic(img_dermaptera, x = 69, y = 7, 
+               height = 0.70, fill = "#003660ff") +
+  ## Orthoptera
+  add_phylopic(img_orthoptera, x = 54, y = 6, 
+               height = 0.70, fill = "#003660ff") +
+  ## Rodentia
+  add_phylopic(flip_phylopic(img_rodentia,
+                             horizontal = TRUE, 
+                             vertical = FALSE), 
+               x = 38, y = 5, 
+               height = 0.70, fill = "#87f7cbff", color = "#003660ff") +
+  ## Reithrodontomys
+  add_phylopic(img_reithrodontomys, 
+               x = 26, y = 4, 
+               height = 0.8, fill = "#87f7cbff", color = "#003660ff") +
+  ## Sciuridae
+  add_phylopic(flip_phylopic(img_sciuridae,
+                             horizontal = TRUE,
+                             vertical = FALSE), 
+               x = 12, y = 3, 
+               height = 0.80, fill = "#87f7cbff", color = "#003660ff") +
+  ## Peromyscus
+  add_phylopic(img_peromyscus, 
+               x = 13, y = 2, 
+               height = 0.80, fill = "#87f7cbff", color = "#003660ff") +
+  ## Pentatomidae
+  add_phylopic(flip_phylopic(img_pentatomidae,
+                             horizontal = TRUE,
+                             vertical = FALSE), 
+               x = 14, y = 1, 
+               height = 0.90, fill = "#003660ff")
+
+
+### Figure Data & Saving
+# View the figure
+fig_sci_prey_occ
+
+# Save to a file as PDF
+ggsave(filename = "figures/fig_sci_prey_occ.pdf",
+       width = 8,
+       height = 7)
+
+# Save to a file as PNG
+ggsave(filename = "figures/fig_sci_prey_occ.png",
+       width = 8,
+       height = 7)
+
 ## Invertebrate Props Scatter ----
 sites_inv_prop <- rbind(proportions, NCOS_proportions) # Combine the proportions for both Dangermond and NCOS
 
@@ -365,6 +477,45 @@ ggsave(filename = "figures/fig_scatter_inv_prop.pdf",
 
 # Save to a file as PNG
 ggsave(filename = "figures/fig_scatter_inv_prop.png",
+       width = 5,
+       height = 6)
+
+## Vertebrate Props Scatter ----
+fig_scatter_vert_prop <- ggplot(sites_inv_prop, aes(x = Site, y = vert_proportion)) +
+  geom_point(alpha = 0) + 
+  geom_jitter(aes(color = Site, fill = Site), 
+              width = 0.15, alpha = 0.5, size = 3, pch = 21) +
+  
+  # Add the overall proportion of invertebrate prey for Dangermond
+  geom_hline(yintercept = mean_vert_prop, linetype = "dashed", color = "darkolivegreen4",linewidth = 1) +
+  geom_text(aes(0, mean_vert_prop, label = round(mean_vert_prop,2), 
+                vjust = -1, hjust = -0.2), color = "darkolivegreen4") +
+  
+  # Add the overall proportion of invertebrate prey for NCOS
+  geom_hline(yintercept = NCOS_mean_vert_prop, linetype = "dashed", color = "#fe7c11", linewidth = 1) +
+  geom_text(aes(0, NCOS_mean_vert_prop, label = round(NCOS_mean_vert_prop,2), 
+                vjust = -1, hjust = -0.2), color = "#fe7c11") +
+  
+  scale_fill_manual(values = c("darkolivegreen3", "#feac11")) +
+  scale_color_manual(values = c("darkolivegreen", "#fe6c11")) +
+  theme_bw() +
+  labs(x = "Study Site", y = "Vertebrate Prey Proportion", color = "Site") +
+  theme(axis.title = element_text(size = 18, face = "bold", 
+                                  family = "serif", color = "#003660ff"),
+        axis.text = element_text(size = 15),
+        legend.title = element_text(face = "bold"),
+        legend.position = "none")
+
+# View the figure
+fig_scatter_vert_prop
+
+# Save to a file as PDF
+ggsave(filename = "figures/fig_scatter_vert_prop.pdf",
+       width = 5,
+       height = 6)
+
+# Save to a file as PNG
+ggsave(filename = "figures/fig_scatter_vert_prop.png",
        width = 5,
        height = 6)
 
